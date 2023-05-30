b50fcdf71280f5598be7c8ec1c007c3b7343dea2
Baokker <1048098862@qq.com>
2023-05-30 21:57:30 +0800

    fbc: init mysql

--- app.py
+++ app.py
@@ -1,22 +1,39 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-import happybase
+# import happybase
+import pymysql
 
 app = Flask(__name__)
 
-# # mysql config
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://yourusername:yourpassword@localhost/yourdatabase'
-# db = SQLAlchemy(app)
+# mysql config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:bbbIII2023@47.101.216.242/bbbiii'
+db = SQLAlchemy(app)
 
 # # hbase
 # conn = happybase.Connection(host='localhost', port=9090)
 # table = conn.table('mytable')
 
-# # mysql orm
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(50), nullable=False)
-#     age = db.Column(db.Integer, nullable=False)
+# mysql orm
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    news_id = db.Column(db.Integer, nullable=False)
+    start_ts = db.Column(db.Integer, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String(50), nullable=False)
+    headline = db.Column(db.Text, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
 
 @app.route('/')
 def hello_world():
@@ -40,6 +57,19 @@ def test_post():
     data = {k: v for k, v in data.items() if v}
     # return json data
     return jsonify(data)
+
+# 获取新闻标题的智能提示
+@app.route('/suggest/newsheadline')
+def search_news():
+    headline = request.args.get('headline')  # 获取用户输入的标题部分
+    amount = request.args.get('amount') or 10  # 如果没有传入 amount 参数，默认返回10条新闻
+
+    # 使用 SQLAlchemy 进行查询
+    news_list = News.query.filter(News.headline.like('%' + headline + '%')).limit(amount).all()
+
+    # 返回查询结果
+    result = [item.headline for item in news_list]
+    return jsonify({'suggestions': result})
 
 if __name__ == '__main__':
     app.run()
